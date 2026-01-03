2323e05d1305fa3f62fabaeddfdd786afde9444e
SRI LAKSHMANAN R <srilakshmananr.ec24@bitsathy.ac.in>
2026-01-03 13:42:14 +0530

    day-4

--- level_7/problem_10.c
+++ level_7/problem_10.c
@@ -1,10 +1,11 @@
 //print the sum of the two  digit odd numbers whose ten's digit is equal to 7
 #include<stdio.h>
-int main(){
-    int x=10;int s=0;
+void two_digit_main(){ int x=10;int s=0;
     for (x;x<100;x++){
        if((x/10==7) &&(x%2!=0)){s+=x;}
     }
-     printf("%d\n",s);
+     printf("%d\n",s);}}
+int main(){
+   two_digit_odd();
     return 0;
 }
