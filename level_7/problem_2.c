1d2483b39e9362e892ae4dfe9d70a2a1114a58f0
SRI LAKSHMANAN R <srilakshmananr.ec24@bitsathy.ac.in>
2026-01-03 13:56:41 +0530

    Day-4

--- level_7/problem_2.c
+++ level_7/problem_2.c
@@ -1,9 +1,10 @@
 //count from five to one  
 #include<stdio.h>
-int main(){
-    int x=5;
+void count_down(){int x=5;
     for (x;x>0;x--){
         printf("%d\n",x);
-    }
+    }}
+int main(){
+    count_down();
     return 0;
 }
