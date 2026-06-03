56f338990b321bf231e23fe93d2a434e3b662a7d
ramlaksh2501 <ramlaksh2501@gmail.com>
2026-06-03 08:08:52 +0530

    need to add flag -pthread while compilling

---
+++ playground/time.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<time.h>
+#include<unistd.h>
+#include<pthread.h>
+void  thread_print(){
+	while(1){
+		printf("hello from the thread!!\n");
+		printf("id of thread:%ld\n PID of thread:%d\n",pthread_self(),getpid());
+	sleep(5);}
+}
+
+
+void normal_print(){
+	while(1){
+	printf("hello from the process!!\n");
+
+	printf(" PID of the process:%d\n",getpid());
+	sleep(5);}
+}
+
+
+int main(){
+	pthread_t id;
+void (*he)();
+	he=&thread_print;
+	pthread_create(&id,NULL,thread_print,NULL);
+	normal_print();
+	return 0;}
